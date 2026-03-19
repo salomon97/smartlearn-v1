46e5f2af86abefa893dc3f48fb2fa58e621a0a80
salomonlamemoire <lamemoire1997@gmail.com>
2026-03-19 05:16:49 +0100

    fix: restore missing Image import in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
 import Link from "next/link";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import UserNav from "@/components/UserNav";
+
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
